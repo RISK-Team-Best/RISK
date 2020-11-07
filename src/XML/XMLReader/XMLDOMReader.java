77299db0e2e27b142ebb63878e12f3a4b9baba6f
libramusician <59710114+libramusician@users.noreply.github.com>
2020-11-07 15:14:55 -0500

    try DOM print

--- src/XML/XMLReader/XMLDOMReader.java
+++ src/XML/XMLReader/XMLDOMReader.java
@@ -1,9 +1,13 @@
 package XML.XMLReader;
 
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
 
 public class XMLDOMReader {
     public XMLDOMReader()
@@ -11,7 +15,28 @@ public class XMLDOMReader {
         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
         try {
             DocumentBuilder builder = factory.newDocumentBuilder();
-            Document doc = builder.parse("test.xml");
+            File f = new File(getClass().getClassLoader().getResource("test.xml").getFile());
+            System.out.println(f.exists());
+            Document doc = builder.parse(f);
+            NodeList booklist = doc.getElementsByTagName("book");
+            for (int i = 0;i<booklist.getLength();i++)
+            {
+                Node node = booklist.item(i);
+                if (node.getNodeType()==Node.ELEMENT_NODE)
+                {
+                    Element element = (Element) node;
+                    String category = element.getAttribute("category");
+                    NodeList info = element.getChildNodes();
+                    System.out.println(element.getTextContent());
+                    for(int j = 0;j<info.getLength();j++)
+                    {
+                        Element element1 = (Element)info.item(j);
+                        //System.out.println(element1.getTagName()+ category + element.getTextContent());
+                        System.out.println(element1.getTextContent());
+                    }
+
+                }
+            }
         }catch (Exception e){}
 
     }
