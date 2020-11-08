e2fb5d73ae297f7cb45dcf5f8e9188845587aa04
libramusician <59710114+libramusician@users.noreply.github.com>
2020-11-08 00:37:40 -0500

    MapEditorV3

---
+++ src/MapEditorV3/MapEditorV3Controller.java
@@ -0,0 +1,40 @@
+package MapEditorV3;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+public class MapEditorV3Controller implements MouseListener {
+    private MapEditorV3Model model;
+    public MapEditorV3Controller(MapEditorV3Model model)
+    {
+        this.model = model;
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        JComponent component = (JComponent) e.getSource();
+        Point p = component.getMousePosition();
+        System.out.println(p.x + "  " +p.y);
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
+}
